ce99485e3aeca7a5b0fe57833468322d934cd3e9
Jeffrey Conradi <jeffreyconradi@gmail.com>
2024-06-02 16:12:57 -0700

    1.1.4 publish

--- src/session/cascading-slot/wincalculator/VideoSlotWinCalculating.ts
+++
@@ -1,5 +0,0 @@
-import {SymbolsCombinationDescribing, VideoSlotWinDetermining} from "pokie";
-
-export interface VideoSlotWinCalculating extends VideoSlotWinDetermining {
-    calculateWin(bet: number, symbolsCombination: SymbolsCombinationDescribing): void;
-}
